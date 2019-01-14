cc376f1adb14419980126cdf1716e0e74a90d2b5
Chloe <cgheazlewood@gmail.com>
2019-01-14 22:22:41 +1100

    Added a sad face image when alternate button pressed, also made faces disappear after 1 second has passed

--- smile_button.py
+++ smile_button.py
@@ -2,5 +2,14 @@
 from microbit import *
 
 while True:
-    if button_a.was_pressed():
+    if button_a.is_pressed():
         display.show(Image.HAPPY)
+        sleep(1000)
+        display.clear()
+        
+        
+    if button_b.is_pressed():
+        display.show(Image.SAD)
+        sleep(1000)
+        display.clear()
+        
